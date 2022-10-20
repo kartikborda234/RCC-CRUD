1a8b16e7836f381f10e62a5fe3852168a3169ce1
kartikborda <kartikborda7@gmail.com>
2022-10-20 17:46:57 +0530

    first comi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
-import {BrowserRouter,Route,Routes} from "react-router-dom"
-import Container from "./demo";
+
 import './App.css'
-import Lif from "./Lifting";
-import HomePage from "./Home";
-import Contact from "./Contact";
-import MyForm from "./Crud";
-import Form from "./Funccrud";
-import Create from "./Create";
-import Read from "./Read";
+
 function App() {
   function parentAlert() {
       alert("hello")
@@ -25,12 +18,7 @@ function App() {
           {/*    <Route path="/" component={HomePage} />*/}
           {/*    <Route path="/contact" component={Contact}/>*/}
           {/*</Switch>*/}
-          <BrowserRouter>
-            <Routes>
-              <Route exact path="/" element={ <Create />} />
-              <Route path="/read" element={<Read />} />
-            </Routes>
-          </BrowserRouter>
+          <div>Kartik buils running</div>
           {/*<MyForm />*/}
           {/*<Form />*/}
           {/*<Create />*/}
